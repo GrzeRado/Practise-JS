8d0775a32cc677529533eae2e09fda85b9c42010
Grzegorz Radomski <rc36@wp.pl>
2018-01-12 06:03:03 +0100

    Add Car object

---
+++ descructuring/descructuring.js
@@ -0,0 +1,16 @@
+//Works after paste it on babel compiler
+
+newCar = {
+    model: Audi,
+    color: Black
+    }
+
+    newCar = {
+    model,
+        color
+    }
+
+    newCar = {
+    model: model1,
+    color: color1
+}
