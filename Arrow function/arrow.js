b789dc30b4cc786699554606d72ddfaf4adc99d7
Grzegorz Radomski <rc36@wp.pl>
2018-01-12 05:38:44 +0100

    Add array and object

---
+++ Arrow function/arrow.js
@@ -0,0 +1,15 @@
+const array = () => [1,2,3]
+console.log(array[0])
+
+
+/*
+const returnObj = () => {
+    return {how: 'prosta',
+    diff: 'kreta'}
+}
+*/
+
+/*
+const kotek = () => 'Filemon'
+console.log('kotek', kotek)
+*/
