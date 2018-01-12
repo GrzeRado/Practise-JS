3c9eb0903c5b38c8bcf8722a79329f3c6b099131
Grzegorz Radomski <rc36@wp.pl>
2018-01-12 07:26:09 +0100

    Add array

--- descructuring/descructuring.js
+++ descructuring/descructuring.js
@@ -1,6 +1,12 @@
 //Works after paste it on babel compiler
 
-const dog = {
+const array = [1,2,kola,5,6]
+const [oneNumer, twoNumer, kola, fiveNumer, sixNumer] = array
+console.log(,,kola,,)
+
+
+
+/* const dog = {
     name: 'Nagar',
     sound: 'wroof'
 }
@@ -9,7 +15,7 @@ const {name, sound} = dog
 
 console.log(name)
 console.log(sound)
-
+*/
 
 /* newCar = {
     model: Audi,
