d92bd83ec3179a53f5ba42553fddee9727278bd4
Grzegorz Radomski <rc36@wp.pl>
2018-01-18 02:02:37 +0100

    add ToDo Folder

---
+++ Callback/callback.js
@@ -0,0 +1,23 @@
+
+function licznik (){
+    console.log('kotek');
+}
+
+setInterval((licznik), 1000)
+
+/*function dodawanie (a,b){
+    return a+b;
+ }
+*/
+/* function odejmowanie (a,b){
+    return a-b;
+ }*/
+/*function mnozenie (a,b) {
+    return a*b;
+}*/
+
+/* function dzielenie(a,b) {
+    return a/b;
+}*/
+
+
